c301b9f900c4904cb3b75f3db179af498855cd47
Piyush Bhatt <gavinharris123@gmail.com>
2021-05-17 14:08:41 +0530

    Basic working app

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,26 @@
+<template>
+    <select @change="onChange" v-model="selected" class="form-select block w-full border p-3 rounded mt-10">
+        <option value="0">Select Country</option>
+        <option v-for="country in countries" :value="country.ID">
+            {{country.Country}}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'CountrySelect',
+    data() {
+        return {
+            selected: 0
+        }
+    },
+    methods: {
+        onChange() {
+            const country = this.countries.find((item) => item.ID === this.selected);
+            this.$emit('get-country', country);
+        }
+    },
+    props: ['countries']
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <main v-if="!loading">
-    
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+    <button class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600" v-if="stats.Country" @click="clearCountryData">Clear Country</button>
   </main>
   <main v-else class="flex flex-col align-center justify-center text-center">
     <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -11,10 +14,13 @@
 </template>
 
 <script>
+import DataTitle from '@/components/DataTitle';
+import DataBoxes from '@/components/DataBoxes';
+import CountrySelect from '@/components/CountrySelect';
 
 export default {
   name: 'Home',
-  components: {},
+  components: { DataTitle, DataBoxes, CountrySelect },
   data() {
     return {
       loading: true,
@@ -31,6 +37,21 @@ export default {
       const data = await res.json();
       return data;
     },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = 'Global';
+      if (data) {
+      this.dataDate = data.Date;
+      this.stats = data.Global;
+      this.countries = data.Countries;
+      this.loading = false;
+    }
+    }
   },
   async created() {
     const data = await this.fetchCovidData();

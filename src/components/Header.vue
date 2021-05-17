24fbdb46eb99d221fdbb9b9fc323e566c3895e06
Piyush Bhatt <gavinharris123@gmail.com>
2021-05-17 12:05:29 +0530

    Initial committ with basic setup

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold"><i class="fa fa-viruses"></i> Covid tracker</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>

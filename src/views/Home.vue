ae33047f6b7e74ee28126094be1ce3897c7b1977
Piyush Bhatt <gavinharris123@gmail.com>
2021-05-17 12:19:27 +0530

    Fetch data from API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="home">
-    Hello
-  </div>
+  <main v-if="!loading">
+    
+  </main>
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="">
+  </main>
 </template>
 
 <script>
@@ -9,5 +15,31 @@
 export default {
   name: 'Home',
   components: {},
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('@/assets/hourglass.gif'),
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    if (data) {
+      this.dataDate = data.Date;
+      this.stats = data.Global;
+      this.countries = data.Countries;
+      this.loading = false;
+    }
+  }
 }
 </script>
